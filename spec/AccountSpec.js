867781f61b46b3d510715e7772bc609147d0a678
RSijelmass <R.Sijelmass@gmail.com>
2017-07-11 11:19:52 +0100

    Initializes with a starting balance

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -1,12 +1,14 @@
 'use strict';
 
-describe 'Account'(function() {
+describe('Account', function() {
 	var account;
 	beforeEach(function() {
 		account = new Account();
 	});
 
 	describe('initialize', function() {
-	
+		it('has a starting balance of 0', function() {
+			expect(account.startBalance).toEqual(0);
+		});	
 	});
 });
---
+++ src/Account.js
@@ -0,0 +1,4 @@
+function Account() {
+	this.startBalance = 0;	//turn into a constant - how to test?
+
+};
