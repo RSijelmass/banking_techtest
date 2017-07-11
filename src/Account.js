314a87d407a9e69d37e4232fb385958e7d0a550a
RSijelmass <R.Sijelmass@gmail.com>
2017-07-11 17:48:28 +0100

    Extracts TransactionHistory into its own file with exports

--- src/Account.js
+++ src/Account.js
@@ -19,34 +19,3 @@ function Account(transactionHistory = new TransactionHistory()) {
 		this.transactionHistory.printer.printStatement();
 	};
 };
-// understands the past money transactions
-function TransactionHistory(printer = new Printer()) {
-	this.printer = printer;
-	this.transactions = [];
-
-	this.recordDeposit = function(amount) {
-		this.transactions.push(amount)
-		var date = this.getCurrentDate()
-	 	printer.printDeposit(date, amount, this.calculateBalance())
-	};	
-	
-	this.recordWithdrawal = function(amount) {
-		this.transactions.push(-amount)
-		var date = this.getCurrentDate()
-	 	printer.printWithdrawal(date, amount, this.calculateBalance())
-};
-
-	this.calculateBalance = function() {
-		var totalBalance = this.transactions.reduce(function(sum, transaction) {
-			return sum + transaction;
-		}, 0);
-		return totalBalance;
-	};
-
-	this.getCurrentDate = function() {
-		var date = new Date();
-		return date.toLocaleDateString();
-	};
-};
-
-
--- src/Printer.js
+++ src/Printer.js
@@ -1,32 +1,32 @@
 // understands the output of transaction history
 (function(exports) {
- function Printer() {
-	HEADLINE =  'date || credit || debit || balance'
-	this.statement = []
-	
-	this.printHeadline = function() {
-		this.statement.unshift(HEADLINE);
-		return HEADLINE;
-	};
+	 function Printer() {
+		HEADLINE =  'date || credit || debit || balance'
+		this.statement = []
+		
+		this.printHeadline = function() {
+			this.statement.unshift(HEADLINE);
+			return HEADLINE;
+		};
 
-	this.printDeposit = function(date, amount, balance) {
-		var statementLine = `${ date } || ${ amount.toFixed(2) } || || ${ balance.toFixed(2) }`
-		this.statement.push(statementLine)
-		return statementLine;
-	};
-	
-	this.printWithdrawal = function(date, amount, balance) {
-		var statementLine = `${ date } || || ${ amount.toFixed(2) } || ${ balance.toFixed(2) }`
-		this.statement.push(statementLine)
-		return statementLine;
-	};
+		this.printDeposit = function(date, amount, balance) {
+			var statementLine = `${ date } || ${ amount.toFixed(2) } || || ${ balance.toFixed(2) }`
+			this.statement.push(statementLine)
+			return statementLine;
+		};
+		
+		this.printWithdrawal = function(date, amount, balance) {
+			var statementLine = `${ date } || || ${ amount.toFixed(2) } || ${ balance.toFixed(2) }`
+			this.statement.push(statementLine)
+			return statementLine;
+		};
 
-	this.printStatement = function() {
-		this.printHeadline();
-		this.statement.forEach(function(line) {
-			console.log(line);
-		});
-	};
- };
+		this.printStatement = function() {
+			this.printHeadline();
+			this.statement.forEach(function(line) {
+				console.log(line);
+			});
+		};
+	 };
 	exports.Printer = Printer;
 })(this);
